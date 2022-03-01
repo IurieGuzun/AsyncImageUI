ea4d5519357b74489b76e48e07eb575eb3e2bee8
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-28 19:19:37 -0500

    Initial Commit

---
+++ AsyncImageUI/AsyncImageUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageUIApp.swift
+//  AsyncImageUI
+//
+//  Created by Iurie Guzun on 2022-02-28.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImageUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AsyncImageUI
+//
+//  Created by Iurie Guzun on 2022-02-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
